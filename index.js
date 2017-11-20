62de9a9454ae97151d92027b953f8564f21129c8
Jennifer Haddock <jentx123@hotmail.com>
2017-11-20 03:04:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,50 @@
+let store = {drivers: [], passengers: [], trips: []};
+
+let driverId = 0;
+
+class Driver {
+  constructor(name){
+    this.id = ++driverId;
+    this.name = name;
+
+    store.drivers.push(this);
+  }
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.userId === this.id
+    })
+  }
+}
+
+let passengerId = 0;
+
+class Passenger{
+  constructor(name){
+    this.id = ++passengerId;
+    this.name = name;
+
+    store.passengers.push(this);
+  }
+}
+
+let tripId = 0;
+
+class Trip{
+  constructor(driver, passenger){
+    this.id = ++tripId;
+    if (driver){
+      this.driverId = driver.id;
+    }
+    if (passenger){
+      this.passengerId = passenger.id;
+    }
+
+    store.trips.push(this);
+  }
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id === this.driverId
+    }.bind(this))
+  }
+}
+
